33fe99896a9c9c720a52a25044ec31bcd6862914
Michał Dudka <michal.dudka@student.wat.edu.pl>
2021-10-27 13:15:50 +0200

    LAB 1 homework

---
+++ Lab1_homework/duddiq_db.sql
@@ -0,0 +1,22 @@
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (1, 'Mona Lisa', 'Leonardo da Vinci', 'portrait', 'idk', 100000, false);
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (2, 'Bitwa pod Grunwaldem', 'Jan Matejko', 'historic', 'a4', 10000, true);
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (3, 'Panny dworskie ', 'Diego Velazquez', 'idk', 'b5', 20000, true);
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (4, 'Gwiaździsta noc', 'Vincent van Gogh', 'aaa', 'xxx', 12345, false);
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (5, 'Trwalosc pamieci', 'Salvador Dali', 'abc', 'xyz', 11453, true);
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (6, 'Impresja', 'Claude Monet', 'sssss', 'wwaw', 647851, false);
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (7, 'Panny z Avinionu', 'Pablo Picasso', 'assdas', 'bgdtor', 235475, true);
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (8, 'Mleczarka', 'Jan Vermeer', 'asadsc', 'hdfde', 25763, true);
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (9, 'Guernica', 'Pablo Picasso', 'asdjkhju', ',ijnjk', 9632, false);
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (10, 'Pocalunek', 'Gustav Klimt', 'jgfgdfs', ',eryetrtw', 741258, true);
+INSERT INTO APP.PICTURES (IMAGE_ID, "NAME", AUTHOR, "TYPE", FORM, USD_PRICE, MUSEUM_PROPERTY) 
+	VALUES (11, 'Narodziny Wenus', 'Sandro Botticelli', 'asojqwijdaks', 'asdqwww', 25468, true);
---
+++ Lab1_homework/src/lab1/DbManager.java
@@ -0,0 +1,57 @@
+package lab1;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public final class DbManager {
+    public static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+    public static final String JDBC_URL="jdbc:derby:D:\\Dane\\Michał\\Studia\\Semestr 5\\JEET\\Lab1_homework\\duddiq_db"; 
+    public static final String QUERY="select * from app.pictures";
+    public static java.sql.Connection conn;
+    
+    private DbManager() { }
+    
+    public static boolean Connect() throws ClassNotFoundException, SQLException {        
+        conn = DriverManager.getConnection(JDBC_URL);
+        if (conn == null) {
+            return false;        
+        } else {
+            return true;
+        }    
+    }
+    
+    public static boolean Disconnect() throws SQLException {
+        if (conn == null) {
+            return false;        
+        } else {            
+            conn.close();
+            return true;        
+        }
+    }
+    
+    public static String getData() throws SQLException {
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery(QUERY);
+        ResultSetMetaData rsmd = rs.getMetaData();
+        String wiersz = new String();
+        
+        int colCount = rsmd.getColumnCount();
+        
+        for (int i = 1; i <= colCount; i++) {            
+            wiersz = wiersz.concat(rsmd.getColumnName(i) + " \t| ");        
+        }        
+        wiersz = wiersz.concat("\r\n");
+        
+        while (rs.next()) {
+            System.out.println("");
+            for (int i = 1; i <= colCount; i++) {                
+                wiersz = wiersz.concat(rs.getString(i) + " \t| ");            
+            }            
+            wiersz = wiersz.concat("\r\n");        
+        }
+        
+        if (stat != null) { stat.close();  }  return wiersz;    }
+}
